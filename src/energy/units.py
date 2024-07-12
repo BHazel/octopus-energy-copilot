8873355aed20da833b7768d129c07c60c29cc3f3
Benedict Hazel <bwhazel@gmail.com>
2024-07-12 08:34:41 +0100

    Restructure units file.

--- src/energy/units.py
+++ src/energy/units.py
@@ -4,30 +4,48 @@ Defines constants for energy units.
 
 from energy import UNIT_REGISTRY
 
-cal = UNIT_REGISTRY.calorie
-
-eV = UNIT_REGISTRY.electron_volt
-
-hp = UNIT_REGISTRY.horsepower
-
-J = UNIT_REGISTRY.joule
-kJ = UNIT_REGISTRY.kilojoule
-MJ = UNIT_REGISTRY.megajoule
-
-kg = UNIT_REGISTRY.kilogram
-tonne = UNIT_REGISTRY.tonne
-
+# Duration
 s = UNIT_REGISTRY.second
 min = UNIT_REGISTRY.minute
 hr = UNIT_REGISTRY.hour
 
+# Energy
+cal = UNIT_REGISTRY.calorie
+eV = UNIT_REGISTRY.electron_volt
+J = UNIT_REGISTRY.joule
+kJ = UNIT_REGISTRY.kilojoule
+MJ = UNIT_REGISTRY.megajoule
+Wh = UNIT_REGISTRY.watt_hour
+kWh = UNIT_REGISTRY.kilowatt_hour
+MWh = UNIT_REGISTRY.megawatt_hour
+
+# Mass
+kg = UNIT_REGISTRY.kilogram
+tonne = UNIT_REGISTRY.tonne
+
+# Power
+hp = UNIT_REGISTRY.horsepower
 W = UNIT_REGISTRY.watt
 kW = UNIT_REGISTRY.kilowatt
 MW = UNIT_REGISTRY.megawatt
 
-Wh = UNIT_REGISTRY.watt_hour
-kWh = UNIT_REGISTRY.kilowatt_hour
-MWh = UNIT_REGISTRY.megawatt_hour
+DURATION_UNITS = [
+    's', 'sec', 'second',
+    'm', 'min', 'minute',
+    'h', 'hr', 'hour'
+]
+
+DURATION_UNIT_MAP = {
+    's': s,
+    'sec': s,
+    'second': s,
+    'm': min,
+    'min': min,
+    'minute': min,
+    'h': hr,
+    'hr': hr,
+    'hour': hr,
+}
 
 ENERGY_UNITS = [
     'cal', 'calorie',
@@ -51,24 +69,6 @@ ENERGY_UNIT_MAP = {
     'wh': Wh,
     'kwh': kWh,
     'mwh': MWh,
-}
-
-DURATION_UNITS = [
-    's', 'sec', 'second',
-    'm', 'min', 'minute',
-    'h', 'hr', 'hour'
-]
-
-DURATION_UNIT_MAP = {
-    's': s,
-    'sec': s,
-    'second': s,
-    'm': min,
-    'min': min,
-    'minute': min,
-    'h': hr,
-    'hr': hr,
-    'hour': hr,
 }
 
 POWER_UNITS = [
